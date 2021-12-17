e3a13f0d099b16071a9548d0781c0141435bfd3a
Junoh Moon <mjo970625@gmail.com>
2021-12-17 20:25:21 +0900

    Fix bugs where not run in powershell without max privilege
    * Force run task in powershell
    * Force run task with maximum privilege

--- register_hevc_task.ps1
+++ register_hevc_task.ps1
@@ -1,6 +1,5 @@
-$action = New-ScheduledTaskAction -Execute 'Add-AppxPackage' -Argument  '-ForceUpdateFromAnyVersion C:\hevc\Microsoft.HEVCVideoExtension_1.0.31823.0_x64__8wekyb3d8bbwe.Appx.appx'
+$action = New-ScheduledTaskAction -Execute powershell -Argument ' -c Add-AppxPackage -ForceUpdateFromAnyVersion C:\hevc\Microsoft.HEVCVideoExtension_1.0.31823.0_x64__8wekyb3d8bbwe.Appx.appx'
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $settings = New-ScheduledTaskSettingsSet
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
 
-Register-ScheduledTask -TaskName 'AutoInstall HEVC extension' -InputObject $task -User $env:UserName -Password '{password}'
+Register-ScheduledTask -TaskName 'AutoInstall HEVC extension' -User $env:UserName -Password '{password}' -RunLevel Highest -Action $action -Settings $settings -Trigger $trigger
