6a4b36fd26f098abed0949fc17d392f236e3a0eb
Junoh Moon <mjo970625@gmail.com>
2021-12-25 20:56:21 +0900

    Start command even if AC is off

--- register_hevc_task.ps1
+++ register_hevc_task.ps1
@@ -1,5 +1,5 @@
 $action = New-ScheduledTaskAction -Execute powershell -Argument ' -c Add-AppxPackage -ForceUpdateFromAnyVersion C:\hevc\Microsoft.HEVCVideoExtension_1.0.31823.0_x64__8wekyb3d8bbwe.Appx.appx'
 $trigger = New-ScheduledTaskTrigger -AtStartup
-$settings = New-ScheduledTaskSettingsSet
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 
 Register-ScheduledTask -TaskName 'AutoInstall HEVC extension' -User $env:UserName -Password '{password}' -RunLevel Highest -Action $action -Settings $settings -Trigger $trigger
